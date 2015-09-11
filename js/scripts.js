0a5dec9c73c500b7ee49532e574688927a1e7490
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-11 15:33:15 -0700

    edit scripts.js and find.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 var findAndReplace = function(text, search, replace) {
   return "a";
 };
+
+
+$(document).ready(function() {
+  $("form#replacer").submit(function(event) {
+    var text = $("input#text").val();
+    var search = $("input#search").val();
+    var replace = $("input#replace").val();
+
+    var result = findAndReplace(text, search, replace);
+
+    $(".results").fadeToggle();
+    $(".data-entry").slideToggle();
+
+    event.preventDefault();
+  });
+});
